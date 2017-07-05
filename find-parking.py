e1091822f8301c315a810c311b5936c8c209f8e3
Oscar Olazabal <oscarolazabal@gmail.com>
2017-07-04 19:13:15 -0700

    finish dispatch function

--- find-parking.py
+++ find-parking.py
@@ -8,12 +8,30 @@ logger.setLevel(logging.DEBUG)
 
 
 def dispatch(intent_request):
-    # Deals with the intent the user specifies
+    '''Deals with the intent the user specifies
+    '''
+    
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'],
+                                                            intent_request['currentIntent']['name']))
+    intent_name = intent_request['currentIntent']['name']
+
+    if intent_name == 'OptimalParking':
+        return optimal_parking(intent_request)
+    elif intent_name == 'SpecificParking':
+        return SpecificParking(intent_request)
+    elif intent_name == 'ListParkingLots':
+        return ListParkingLots(intent_request)
+    elif intent_name == 'GetDirections':
+        return GetDirections(intent_request)
+    else:
+        raise Exception('Intent with name {} not supported'.format(intent_name))
+    
+
 
 def lambda_handler(event, context):
     '''Main handler
-    event is used to pass in event data.
-    context is used to provide runtime information.
+    Event is used to pass in event data. Seems to be stored as nested dictionaries.
+    Context is used to provide runtime information.
     '''
 
     logger.debug('event.bot.name={}'.format(event['bot']['name']))
