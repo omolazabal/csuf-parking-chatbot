2549457c4a1dd615115c9e91cbef5e699c1b374d
Oscar Olazabal <oscarolazabal@gmail.com>
2017-07-11 21:10:22 -0700

    more PEP8 guidelines

--- lambda-function/helper.py
+++ lambda-function/helper.py
@@ -29,16 +29,18 @@ def build_validation_result(is_valid, violated_slot, message_content):
 
 def validate_parking_lot(parking_lot):
     """Checks whether the user's requested lot is valid"""
+
     parking_lots = [
         'evfree church', 'state college', 'a and g', 'g',
         'a', 'eastside', 'nutwood', 'brea mall', 'all'
     ]
 
     if parking_lot is not None and parking_lot.lower() not in parking_lots:
-        return build_validation_result(False,
-                                       'ParkingLot',
-                                       'I do not know of any information'
-                                       'regarding {}, try a different '
-                                       'parking lot.'.format(parking_lot))
+        return build_validation_result(
+            False,
+            'ParkingLot',
+            'I do not know of any information regarding {}, try a different'
+            'parking lot.'.format(parking_lot)
+        )
 
     return build_validation_result(True, None, None)
