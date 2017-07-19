a4d06c004363abb22735688157a83110416a433e
Oscar Olazabal <oscarolazabal@gmail.com>
2017-07-19 07:34:49 -0700

    move the creation of the messages to the message file

--- lambda-function/fulfillment.py
+++ lambda-function/fulfillment.py
@@ -4,6 +4,7 @@
 import json
 import response
 import helper
+import message as msg
 import lambda_function as lamfunc
 
 
@@ -79,36 +80,12 @@ def list_parking(intent_request):
     if source == 'FulfillmentCodeHook':
         lamfunc.logger.debug('request for lot list')
 
-        # Obtain data and format into message
-        lot_list = helper.get_available_lots()
-
-        if not lot_list['ClosedLots']:
-            available_lots = lot_list['AvailableLots'][0]
-            del lot_list['AvailableLots'][0]
-            for lot in lot_list['AvailableLots']:
-                available_lots += (', ' + lot)
-            message = 'All parking locations are open today! You can park' \
-                      ' at {}.'.format(available_lots)
-        else:
-            available_lots = lot_list['AvailableLots'][0]
-            del lot_list['AvailableLots'][0]
-            for lot in lot_list['AvailableLots']:
-                available_lots += (', ' + lot)
-
-            closed_lots = lot_list['ClosedLots'][0]
-            del lot_list['ClosedLots'][0]
-            for lot in lot_list['ClosedLots']:
-                closed_lots += (', ' + lot)
-
-            message = 'Today you can park at: {}. You cannot park at: ' \
-                      '{}.'.format(available_lots, closed_lots)
-
         return response.close(
             intent_request['sessionAttributes'],
             'Fulfilled',
             {
                 'contentType': 'PlainText',
-                'content': message
+                'content': msg.list_lots_msg()
             }
         )
 
@@ -179,40 +156,13 @@ def specific_parking(intent_request):
         # information for.
         session_attributes['lastParkingRequest'] = parking_request
 
-        # Obtain and format data into message
-        parking_data = helper.scrape_data()
-        lot_name = parking_lot.replace(' ', '')
-
-        if lot_name == 'all':
-            message = 'Here are the available spaces for all locations: ' \
-                  'Nutwood Structure ({}), State College Structure({}), ' \
-                  'Eastside Structure ({}), Lot A & G ({}), ' \
-                  'EvFree Church ({}), Brea Mall ({})'.format(
-                      parking_data['NutwoodStructure']['AvailableSpaces'],
-                      parking_data['StateCollegeStructure']['AvailableSpaces'],
-                      parking_data['EastsideStructure']['AvailableSpaces'],
-                      parking_data['LotA&G']['AvailableSpaces'],
-                      parking_data['EvFreeChurch']['AvailableSpaces'],
-                      parking_data['BreaMall']['AvailableSpaces']
-                  )
-        elif parking_data[lot_name]['AvailableSpaces'] == 'Closed':
-            message = '{} is currently close. It is open on {}.'.format(
-                parking_lot,
-                parking_data[lot_name]['Date']
-            )
-        else:
-            message = "{} currently has {} available parking spaces.".format(
-                parking_lot,
-                parking_data[lot_name]['AvailableSpaces']
-            )
-
         # End the intent.
         return response.close(
             session_attributes,
             'Fulfilled',
             {
                 'contentType': 'PlainText',
-                'content': message
+                'content': msg.specific_parking_msg(parking_lot)
             }
         )
 
@@ -312,36 +262,13 @@ def get_directions(intent_request):
         helper.try_ex(lambda: session_attributes.pop('currentParkingRequest'))
         helper.try_ex(lambda: session_attributes.pop('lastParkingRequest'))
 
-        # Obtain and format data into message
-        parking_data = helper.scrape_data()
-        lot_name = parking_lot.replace(' ', '')
-
-        if lot_name == 'all':
-            message = 'Here are the directions for all locations: ' \
-                  'Nutwood Structure ({}), State College Structure({}), ' \
-                  'Eastside Structure ({}), Lot A & G ({}), ' \
-                  'EvFree Church ({}), Brea Mall ({})'.format(
-                      parking_data['NutwoodStructure']['Directions'],
-                      parking_data['StateCollegeStructure']['Directions'],
-                      parking_data['EastsideStructure']['Directions'],
-                      parking_data['LotA&G']['Directions'],
-                      parking_data['EvFreeChurch']['Directions'],
-                      parking_data['BreaMall']['Directions']
-                  )
-
-        else:
-            message = 'Here are the directions to {}: {}'.format(
-                  parking_lot,
-                  parking_data[lot_name]['Directions']
-            )
-
         # End the intent.
         return response.close(
             session_attributes,
             'Fulfilled',
             {
                 'contentType': 'PlainText',
-                'content': message
+                'content': msg.directions_msg(parking_lot)
             }
         )
 
